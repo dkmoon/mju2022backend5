23387a57c541a2772570ba25ecc4104c1b44abac
DK Moon <dkmoon@gmail.com>
2022-10-11 02:24:21 +0900

    tcpserver-select 추가

---
+++ tcpserver-select/tcpserver-select.cpp
@@ -0,0 +1,257 @@
+﻿#include <cstring>
+#include <iostream>
+#include <list>
+#include <map>
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+using namespace std;
+
+// ws2_32.lib 를 링크한다.
+#pragma comment(lib, "Ws2_32.lib")
+
+static unsigned short SERVER_PORT = 27015;
+
+
+class Client {
+public:
+    SOCKET sock;  // 이 클라이언트의 active socket
+
+    ~Client() {
+        cout << "Client destroyed. Socket: " << sock << endl;
+    }
+
+    // TODO: 여기를 채운다.
+};
+
+
+// 소켓으로부터 Client 객체 포인터를 얻어내기 위한 map
+// 소켓을 key 로 Client 객체 포인터를 value 로 집어넣는다. (shared_ptr 을 사용한다.)
+// 나중에 key 인 소켓으로 찾으면 연결된 Client 객체 포인터가 나온다.
+// key 인 소켓으로 지우면 해당 엔트리는 사라진다.
+// key 목록은 소켓 목록이므로 현재 남아있는 소켓들이라고 생각할 수 있다.
+map<SOCKET, shared_ptr<Client> > activeClients;
+
+
+SOCKET createPassiveSocket() {
+    // TCP socket 을 만든다.
+    SOCKET passiveSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (passiveSock == INVALID_SOCKET) {
+        cerr << "socket failed with error " << WSAGetLastError() << endl;
+        return 1;
+    }
+
+    // socket 을 특정 주소, 포트에 바인딩 한다.
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    int r = bind(passiveSock, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    if (r == SOCKET_ERROR) {
+        cerr << "bind failed with error " << WSAGetLastError() << endl;
+        return 1;
+    }
+
+    // TCP 는 연결을 받는 passive socket 과 실제 통신을 할 수 있는 active socket 으로 구분된다.
+    // passive socket 은 socket() 뒤에 listen() 을 호출함으로써 만들어진다.
+    // active socket 은 passive socket 을 이용해 accept() 를 호출함으로써 만들어진다.
+    r = listen(passiveSock, 10);
+    if (r == SOCKET_ERROR) {
+        cerr << "listen faijled with error " << WSAGetLastError() << endl;
+        return 1;
+    }
+
+    return passiveSock;
+}
+
+
+bool processClient(shared_ptr<Client> client) {
+    SOCKET activeSock = client->sock;
+
+    // 길이 정보를 받기 위해서 4바이트를 읽는다.
+                // network byte order 로 전성되기 때문에 ntohl() 을 호출한다.
+    int dataLenNetByteOrder;
+    int offset = 0;
+    while (offset < 4) {
+        int r = recv(activeSock, ((char*)&dataLenNetByteOrder) + offset, 4 - offset, 0);
+        if (r == SOCKET_ERROR) {
+            cerr << "recv failed with error " << WSAGetLastError() << endl;
+            return false;
+        }
+        else if (r == 0) {
+            // 메뉴얼을 보면 recv() 는 소켓이 닫힌 경우 0 을 반환함을 알 수 있다.
+            // 따라서 r == 0 인 경우도 loop 을 탈출하게 해야된다.
+            cerr << "Socket closed: " << activeSock << endl;
+            return false;
+        }
+        offset += r;
+    }
+    int dataLen = ntohl(dataLenNetByteOrder);
+    cout << "Received length info: " << dataLen << endl;
+
+    // socket 으로부터 데이터를 받는다.
+    // TCP 는 연결 기반이므로 누가 보냈는지는 accept 시 결정되고 그 뒤로는 send/recv 만 호출한다.
+    shared_ptr<char> buf{ new char[dataLen] };
+    offset = 0;
+    while (offset < dataLen) {
+        int r = recv(activeSock, buf.get() + offset, dataLen - offset, 0);
+        if (r == SOCKET_ERROR) {
+            cerr << "recv failed with error " << WSAGetLastError() << endl;
+            return false;
+        }
+        else if (r == 0) {
+            // 메뉴얼을 보면 recv() 는 소켓이 닫힌 경우 0 을 반환함을 알 수 있다.
+            // 따라서 r == 0 인 경우도 loop 을 탈출하게 해야된다.
+            break;
+        }
+        cout << "Received " << r << " bytes" << endl;
+        offset += r;
+    }
+
+    return true;
+}
+
+
+int main()
+{
+    int r = 0;
+
+    // Winsock 을 초기화한다.
+    WSADATA wsaData;
+    r = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (r != NO_ERROR) {
+        cerr << "WSAStartup failed with error " << r << endl;
+        return 1;
+    }
+
+    // passive socket 을 만들어준다.
+    SOCKET passiveSock = createPassiveSocket();
+
+    // 서버는 사용자가 중단할 때까지 프로그램이 계속 동작해야된다.
+    // 따라서 loop 으로 반복 처리한다.
+    while (true) {
+        // select 를 이용해 읽기 이벤트와 예외 이벤트가 발생하는 소켓을 알아낼 것이다.
+        // fd_set 은 C/C++ 에서 정한 것이 아니라 typedef 로 정해진 custom type 이다.
+        // 그런데 우리는 구체적인 구현은 신경쓰지 않아도 되고 대신 FD_XXX() 의 매크로 함수를 이용해 접근할 것이다.
+        fd_set readSet, exceptionSet;
+
+        // 위의 socket set 을 초기화한다.
+        FD_ZERO(&readSet);
+        FD_ZERO(&exceptionSet);
+
+        // select 의 첫번째 인자는 max socket 번호에 1을 더한 값이다.
+        // 따라서 max socket 번호를 계산한다.
+        SOCKET maxSock = -1;
+
+        // passive socket 은 기본으로 각 socket set 에 포함되어야 한다.
+        FD_SET(passiveSock, &readSet);
+        FD_SET(passiveSock, &exceptionSet);
+        maxSock = max(maxSock, passiveSock);
+
+        // 현재 남아있는 active socket 들에 대해서도 모두 set 에 넣어준다.
+        for (auto &entry : activeClients) {
+            SOCKET activeSock = entry.first;
+            FD_SET(activeSock, &readSet);
+            FD_SET(activeSock, &exceptionSet);
+            maxSock = max(maxSock, activeSock);
+        }
+
+        // select 를 해준다. timeout 없이 이벤트가 발생할 때까지 기다릴 것이다.
+        r = select(maxSock + 1, &readSet, NULL, &exceptionSet, NULL);
+
+        // 회복할 수 없는 오류이다. 서버를 중단한다.
+        if (r == SOCKET_ERROR) {
+            cerr << "select failed: " << WSAGetLastError() << endl;
+            break;
+        }
+
+        // passive socket 이 readable 하다면 이는 새 연결이 들어왔다는 것이다.
+        // 새 클라이언트 객체를 동적으로 만들고 
+        if (FD_ISSET(passiveSock, &readSet)) {
+            // passive socket 을 이용해 accept() 를 한다.
+            // accept() 는 blocking 이지만 우리는 이미 select() 를 통해 새 연결이 있음을 알고 accept() 를 호출한다.
+            // 따라서 여기서는 blocking 되지 않는다.
+            // 연결이 완료되고 만들어지는 소켓은 active socket 이다.
+            cout << "Waiting for a connection" << endl;
+            struct sockaddr_in clientAddr;
+            int clientAddrSize = sizeof(clientAddr);
+            SOCKET activeSock = accept(passiveSock, (sockaddr*)&clientAddr, &clientAddrSize);
+
+            // accpet() 가 실패하면 해당 연결은 이루어지지 않았음을 의미한다.
+            // 그 여결이 잘못된다고 하더라도 다른 연결들을 처리해야되므로 에러가 발생했다고 하더라도 계속 진행한다.
+            if (activeSock == INVALID_SOCKET) {
+                cerr << "accept failed with error " << WSAGetLastError() << endl;
+                return 1;
+            }
+            else {
+                // 새로 client 객체를 만든다.
+                shared_ptr<Client> newClient{ new Client };
+                newClient->sock = activeSock;
+
+                // socket 을 key 로 하고 해당 객체 포인터를 value 로 하는 map 에 집어 넣는다.
+                activeClients.insert(make_pair(activeSock, newClient));
+                
+                // 로그를 찍는다.
+                char strBuf[1024];
+                inet_ntop(AF_INET, &(clientAddr.sin_addr), strBuf, sizeof(strBuf));
+                cout << "New client from " << strBuf << ":" << ntohs(clientAddr.sin_port) << ". "
+                     << "Socket: " << activeSock << endl;
+            }
+        }
+
+        // 오류 이벤트가 발생하는 소켓의 클라이언트는 제거한다.
+        // activeClients 를 순회하는 동안 그 내용을 변경하면 안되니 지우는 경우를 위해 별도로 list 를 쓴다.
+        list<SOCKET> toDelete;
+        for (auto& entry : activeClients) {
+            SOCKET activeSock = entry.first;
+            shared_ptr<Client> client = entry.second;
+
+            if (FD_ISSET(activeSock, &exceptionSet)) {
+                cerr << "Exception on socket " << activeSock << endl;
+
+                // 소켓을 닫는다.
+                closesocket(activeSock);
+
+                // 지울 대상에 포함시킨다.
+                // 여기서 activeClients 에서 바로 지우지 않는 이유는 현재 activeClients 를 순회중이기 때문이다.
+                toDelete.push_back(activeSock);
+
+                // 소켓을 닫은 경우 더 이상 처리할 필요가 없으니 아래 read 작업은 하지 않는다.
+                continue;
+            }
+
+            // 읽기 이벤트가 발생하는 소켓의 경우 recv() 를 처리한다.
+            // 주의: 아래는 여전히 recv() 에 의해 blocking 이 발생할 수 있다.
+            //       우리는 이를 producer-consumer 형태로 바꿀 것이다.
+            if (FD_ISSET(activeSock, &readSet)) {
+                bool successful = processClient(client);
+                if (successful == false) {
+                    closesocket(activeSock);
+                    toDelete.push_back(activeSock);
+                }
+            }
+        }
+
+        // 이제 지울 것이 있었다면 지운다.
+        for (auto& closedSock : toDelete) {
+
+            // 맵에서 지우고 객체도 지워준다.
+            // shared_ptr 을 썼기 때문에 맵에서 지워서 더 이상 사용하는 곳이 없어지면 객체도 지워진다.
+            activeClients.erase(closedSock);
+        }
+    }
+
+    // 연결을 기다리는 passive socket 을 닫는다.
+    r = closesocket(passiveSock);
+    if (r == SOCKET_ERROR) {
+        cerr << "closesocket(passive) failed with error " << WSAGetLastError() << endl;
+        return 1;
+    }
+
+    // Winsock 을 정리한다.
+    WSACleanup();
+    return 0;
+}
